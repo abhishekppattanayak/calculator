7677e0d5176a82761a3cf42063fab135b57b882c
Abhishek Pattanayak <2021.abhishek.pattanayak@ves.ac.in>
2023-12-27 15:18:22 +0530

    Add funtionality

--- script.js
+++ script.js
@@ -6,13 +6,60 @@ const div = (a, b) => {
     return a/b;
 }
 
-const operate = (a, b, c) => {
+const operate = (a, c, b) => {
     a = parseFloat(a)
     b = parseFloat(b)
-    if(c==='+') return add(a, b)
-    if(c==='-') return sub(a, b)
-    if(c==='*') return mul(a, b)
-    if(c==='/') return div(a, b)
+    if(c==='+')
+        return add(a, b)
+    if(c==='-') 
+        return sub(a, b)
+    if(c==='*')
+        return mul(a, b)
+    if(c==='/')
+        return div(a, b)
 }
 
 document.querySelector('footer > span').textContent = `Copyright © ${new Date().getFullYear()} `;
+
+let firstNumber="" , secondNumber="" , operator="";
+
+const display = () => document.querySelector('main>div').textContent = `${firstNumber} ${operator} ${secondNumber}`;
+
+const number = button => {
+    if(operator==''){
+        firstNumber += button.textContent;
+        console.log(firstNumber, operator, secondNumber);
+    }else {
+        secondNumber += button.textContent;
+        console.log(firstNumber, operator, secondNumber);
+    }
+    display();
+}
+
+const operation = button => {
+    if(secondNumber==""){
+        operator = button.textContent;
+        console.log(firstNumber, operator, secondNumber);
+    }else{
+        firstNumber = `${operate(firstNumber, operator, secondNumber)}`;
+        operator = button.textContent;
+        secondNumber = '';
+        console.log(firstNumber, operator, secondNumber);
+    }
+    display();
+}
+
+const equal = () => {
+    if(secondNumber!=""){
+        firstNumber = `${operate(firstNumber, operator, secondNumber)}`;
+        secondNumber = ""
+        document.querySelector('main>div').textContent = firstNumber;
+    }
+}
+
+const clearDisplay = () => { 
+    firstNumber = "";
+    operator = "";
+    secondNumber = "";
+    display();
+}
